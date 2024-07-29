558ce4f207586dc3b49e9ad09529a713ac6f7412
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-07-29 12:17:35 +0900

    Create log in modal

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,29 @@
-import { Box, HStack, Button } from '@chakra-ui/react';
-import { FaAirbnb } from 'react-icons/fa';
+import {
+  Box,
+  HStack,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  VStack,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { FaAirbnb, FaUser, FaLock } from 'react-icons/fa';
 import { Outlet } from 'react-router-dom';
 
 export default function Root() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
     <Box>
       <HStack justifyContent={'space-between'} py={5} px={10} borderBottomWidth={1}>
@@ -10,9 +31,37 @@ export default function Root() {
           <FaAirbnb size='38px' />
         </Box>
         <HStack spacing={2}>
-          <Button>Login</Button>
+          <Button onClick={onOpen}>Log in</Button>
           <Button colorScheme='red'>Sign up</Button>
         </HStack>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <InputGroup>
+                  <InputLeftElement children={<FaUser />}></InputLeftElement>
+                  <Input placeholder='Username'></Input>
+                </InputGroup>
+                <InputGroup>
+                  <InputLeftElement children={<FaLock />}></InputLeftElement>
+                  <Input type={show ? 'text' : 'password'} placeholder='Password'></Input>
+                  <InputRightElement>
+                    <Button onClick={handleClick} size='xs' variant={'link'}>
+                      {show ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </VStack>
+              <Button colorScheme='red' w='100%' my={5}>
+                Log in
+              </Button>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       </HStack>
       <Outlet />
     </Box>
