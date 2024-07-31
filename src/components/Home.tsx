1f5af28fc22ae59d7d1d81bfba6e75d489eb652f
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-07-31 14:16:59 +0900

    feat(RoomList): Connect to backend API

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,54 @@
-import { Box, Grid, Skeleton, SkeletonText } from '@chakra-ui/react';
+import { Grid } from '@chakra-ui/react';
 import Room from './Room';
 import RoomSkeleton from './RoomSkeleton';
+import { useEffect, useState } from 'react';
+
+interface IPhoto {
+  pk: string;
+  file: string;
+  description: string;
+}
+
+interface IRoom {
+  pk: number;
+  name: string;
+  country: string;
+  city: string;
+  description: string;
+  room_type: string;
+  bedrooms: number;
+  price: number;
+  rating: number;
+  number_of_reviews: number;
+  is_owner: boolean;
+  photos: IPhoto[];
+}
+
+interface ILinkInfo {
+  current_page: number;
+  total_pages: number;
+  next_link: null | string;
+  prev_link: null | string;
+  count: number;
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [linkInfo, setLinkInfo] = useState<ILinkInfo>({} as ILinkInfo);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const fetchRooms = async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/v1/rooms');
+    const data = await response.json();
+    setRooms(data.content);
+    setLinkInfo(data.page);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
+  console.log(rooms);
+
   return (
     <Grid
       mt={10}
@@ -17,13 +63,33 @@ export default function Home() {
         '2xl': 'repeat(5, 1fr)',
       }}
     >
-      <RoomSkeleton />
-      <Room />
-      {/* 
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 2, 2, 3, 4, 6, 7, 8].map((index) => (
-        <Room key={index} />
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+      {rooms.map((room) => (
+        <Room
+          imageUrl={room.photos[0].file}
+          type={room.room_type}
+          city={room.city}
+          rating={room.rating}
+          totalReviews={room.number_of_reviews}
+          name={room.name}
+          bedrooms={room.bedrooms}
+          price={room.price}
+        />
       ))}
-       */}
+      ;
     </Grid>
   );
 }
--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -10,15 +10,39 @@ import {
 } from '@chakra-ui/react';
 import { FaRegHeart, FaStar } from 'react-icons/fa';
 
-export default function Room({ index }) {
+interface IRoomProps {
+  imageUrl: string;
+  type: string;
+  city: string;
+  rating: number;
+  totalReviews: number;
+  name: string;
+  bedrooms: number;
+  price: number;
+}
+
+export default function Room({
+  imageUrl,
+  type,
+  city,
+  rating,
+  totalReviews,
+  name,
+  bedrooms,
+  price,
+}: IRoomProps) {
   const gray = useColorModeValue('gray600', 'gray.300');
   return (
     <VStack spacing={-0.5} alignItems={'flex-start'}>
-      <Box key={index} overflow={'hidden'} position='relative' rounded='xl' mb={2.5}>
-        <Image
-          minH='250'
-          src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE4MjI4MjI3OTY3Mjg4ODY3Nw%3D%3D/original/63897870-3cbf-4d5c-9233-38376d894a97.jpeg'
-        />
+      <Box
+        overflow={'hidden'}
+        position='relative'
+        rounded='xl'
+        mb={2.5}
+        h='280px'
+        w='100%'
+      >
+        <Image src={imageUrl} objectFit='cover' w='100%' h='100%' />
         <IconButton
           size={'lg'}
           aria-label='Add to wishlist'
@@ -32,27 +56,24 @@ export default function Room({ index }) {
       </Box>
       <Grid templateColumns={'10fr 1fr'}>
         <Text noOfLines={1} fontSize='md' fontWeight='semibold'>
-          Flat in London
+          {type} in {city}
         </Text>
         <HStack spacing={1} color={gray}>
           <FaStar size={15} />
-          <Text>5.0</Text>
-          <Text>(4)</Text>
+          <Text>{rating}</Text>
+          <Text>({totalReviews})</Text>
         </HStack>
       </Grid>
       <Text noOfLines={1} fontSize='sm' color={gray}>
-        BAT-3-A Soho Terrace, gorgeous 1 bedroom apt, A/C
+        {name}
       </Text>
       <Text noOfLines={1} fontSize='sm' color={gray}>
-        2 beds
-      </Text>
-      <Text fontSize='sm' color={gray}>
-        Professional Host
+        {bedrooms} bedrooms
       </Text>
       <HStack spacing={1} pt={1.5}>
         <Text fontSize='sm' color={gray}>
           <Text fontWeight='semibold' as='text' mr={1}>
-            $64
+            ${price}
           </Text>
           night
         </Text>
@@ -60,7 +81,7 @@ export default function Room({ index }) {
           ·
         </Text>
         <Text fontSize='sm' color={gray} decoration='underline'>
-          $64 total
+          ${price} total
         </Text>
       </HStack>
     </VStack>
