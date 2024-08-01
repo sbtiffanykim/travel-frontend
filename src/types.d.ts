2b5825610b7450b5c0d9a57468db7e0f89ad532c
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-08-01 22:21:45 +0900

    refactor(types): Move interfaces to types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,28 @@
+export interface IPhoto {
+  pk: string;
+  file: string;
+  description: string;
+}
+
+export interface IRoomList {
+  pk: number;
+  name: string;
+  country: string;
+  city: string;
+  description: string;
+  room_type: string;
+  bedrooms: number;
+  price: number;
+  rating: number;
+  number_of_reviews: number;
+  is_owner: boolean;
+  photos: IPhoto[];
+}
+
+export interface ILinkInfo {
+  current_page: number;
+  total_pages: number;
+  next_link: null | string;
+  prev_link: null | string;
+  count: number;
+}
