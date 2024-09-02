c3c11df6a84f48c43e9840e4f1d5409651763b8f
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-09-02 23:03:52 +0900

    feat: Add toast for unauthorized access

--- src/lib/useRequireHost.ts
+++ src/lib/useRequireHost.ts
@@ -1,15 +1,22 @@
 import { useEffect } from 'react';
 import useUser from './useUser';
 import { useNavigate } from 'react-router-dom';
+import { useToast } from '@chakra-ui/react';
 
 export default function useRequireHost() {
   const { user, userLoading } = useUser();
+  const toast = useToast();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!userLoading) {
       if (!user?.is_host) {
         navigate('/');
+        toast({
+          title: 'Permission Denied',
+          description: 'Only hosts can access this page',
+          status: 'error',
+        });
       }
     }
   }, [userLoading, navigate]);
